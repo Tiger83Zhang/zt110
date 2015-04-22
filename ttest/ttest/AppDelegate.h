376e5a8206b7867a1b2c49ca5eadbc8414a474c9
Tiger83Zhang <zhangsx@ZhangSxdeMacBook-Pro.local>
2015-04-22 15:21:55 +0800

    create
    创建

---
+++ ttest/ttest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ttest
+//
+//  Created by ZhangSx on 15/4/22.
+//  Copyright (c) 2015年 ZhangSx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ttest/ttest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ttest
+//
+//  Created by ZhangSx on 15/4/22.
+//  Copyright (c) 2015年 ZhangSx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
